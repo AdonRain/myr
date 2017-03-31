24f0870b1b52a960f7157cc2e5f333e5eefe4bdc
雷东升 <leidongsheng@luojilab.com>
2017-03-31 14:04:59 +0800

    first commit

---
+++ R/utils.R
@@ -0,0 +1,17 @@
+## Personal R Tool Kit
+
+#' First line is title
+#'
+#' This is a brief description.
+#'
+#' Anything else after the description goes to the Details section.
+
+import <- function(package) {
+  new.package <- package[!(package %in% installed.packages()[, "Package"])]
+
+  if (length(new.package)){
+    install.packages(new.package, dependencies = TRUE)
+  }
+
+  sapply(package, require, character.only = TRUE)
+}
