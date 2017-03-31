3e09424e9cd9e51cbf5e035db7cae387a6cb4d47
雷东升 <leidongsheng@luojilab.com>
2017-03-31 17:24:48 +0800

    save

--- R/main.R
+++ R/main.R
@@ -6,6 +6,15 @@
 #'
 #' Anything else after the description goes to the Details section.
 
+#'
+#' @param x a numeric vector giving the values of the random variable.
+#' @param probs optional, a numeric vector giving the probabilities
+#'   corresponding to each x value. If not specific, assumes all outcomes
+#'   are equally likely
+#' @export
+#' @return An S3 object of class rv.
+#' @examples
+
 myr_library <- function(package) {
   new.package <- package[!(package %in% installed.packages()[, "Package"])]
 
